bf979e623d3ce968920f0efc999f3fc7b44aa5d1
Olivia Gelinas <oliviagelinas1@gmail.com>
2018-03-16 19:09:29 -0400

    back to previous verison

--- TP3/question2.py
+++ TP3/question2.py
@@ -51,41 +51,37 @@ class myAgent(object):
 
     def act(self, x, epsilon=0.1):
         # if else avec epsilon qui est prob de choisir un action au hasard
-        decision = random.random()
-        if decision < epsilon:
-            q = self.Q(x).type(torch.FloatTensor)
-            qData = q.data
-            qMax = qData.max(0)[1] # l'indice est en [1]
-            res = Variable(torch.LongTensor(qMax))
-            return res
+        sample = random.random()
+        if sample > epsilon:
+            maxim = self.Q(x).data.max(0)[1]
+            return Variable(torch.LongTensor(maxim))
+
         else:
             return Variable(torch.LongTensor([random.randrange(2)]))
 
     def backward(self, transitions):
         batch = Transition(*zip(*transitions))
-        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None,
-                                                    batch.next_state)))
-        non_final_next_states = Variable(torch.cat([s for s in batch.next_state
-                                                    if s is not None]),
-                                         volatile=True)
-        state_batch = Variable(torch.cat(batch.state))
-        action_batch = Variable(torch.cat(batch.action))
-        reward_batch = Variable(torch.cat(batch.reward))
 
-        state_action_values = self.Q(state_batch).gather(1, action_batch.view(-1,1))
+        batch_state = Variable(torch.cat(batch.state))
+        batch_action = Variable(torch.cat(batch.action))
+        batch_reward = Variable(torch.cat(batch.reward))
+        batch_next_state = Variable(torch.cat(batch.next_state))
 
-        next_state_values = Variable(torch.zeros(128).type(torch.Tensor))
-        next_state_values[non_final_mask] = self.Q(non_final_next_states).max(1)[0]
+        current_actions = self.Q(batch_state)
+        current_q_values = current_actions.gather(1, batch_action.view(-1, 1))
 
-        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+        max_next_q_values = self.target_Q(batch_next_state).detach().max(1)[1]
+        expected_q_values = (batch_reward.type(torch.FloatTensor) + (self.gamma * max_next_q_values.type(torch.FloatTensor))).view(-1, 1)
 
-        # Compute Huber loss
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+        assert current_q_values.shape == expected_q_values.shape  # les deux matrices doivent être de la même taille pour que ça marche
+
+        loss = F.mse_loss(current_q_values, expected_q_values)
+
+        soft_update(self.target_Q, self.Q, self.gamma)
 
         self.optimizer.zero_grad()
         loss.backward()
-        for param in self.Q.parameters():
-            param.grad.data.clamp_(-1, 1)
+
         self.optimizer.step()
 
 
@@ -133,7 +129,6 @@ for i in range(5000):
         obs_input = Variable(torch.from_numpy(obs).type(torch.FloatTensor))
         action = agent.act(obs_input, epsilon)
         #env.render() #Pour visualiser les mouvement a l'écran.
-        tmp = action.data.numpy()
         next_obs, reward, done, _ = env.step(action.data.numpy()[0])
         memory.push(obs_input.data.view(1,-1), action.data,
                     torch.from_numpy(next_obs).type(torch.FloatTensor).view(1,-1), torch.Tensor([reward]),torch.Tensor([done]))
